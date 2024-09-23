41b202efbe40c02ae42663a8deff903bf4f5429e
weixcss <weixcss@gmail.com>
2024-09-23 17:17:55 -0400

    changes to urls.py

--- quotes/urls.py
+++ quotes/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.quote, name='quotes'),  # http://127.0.0.1:8000/quotes/
+    path('quotes/', views.quote, name='quotes'),  # http://127.0.0.1:8000/quotes/
     path('quotes/quote/', views.quote, name='quote'),  # http://127.0.0.1:8000/quotes/quote
     path('quotes/show_all/', views.show_all, name='show_all'),  # http://127.0.0.1:8000/quotes/show_all/
     path('quotes/about/', views.about, name='about'),  # http://127.0.0.1:8000/quotes/about/
