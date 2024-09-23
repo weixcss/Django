f00922aa04f30af1d14f71125fe3582c70b67349
weixcss <weixcss@gmail.com>
2024-09-23 16:40:29 -0400

    fixes

--- cs412/settings.py
+++ cs412/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'cs412app.apps.Cs412AppConfig',
+    'quotes.apps.QuotesConfig',
 ]
 
 MIDDLEWARE = [
