68e9a70d193bd715a808fa594a120b77b5c55883
weixcss <weixcss@gmail.com>
2024-09-24 11:50:49 -0400

    updates to formdata

--- formdata/urls.py
+++ formdata/urls.py
@@ -3,6 +3,9 @@ from django.conf import settings
 from . import views
 
 urlpatterns = [ 
-    path(r'', views.show_form, name="show_form"),
-    path(r'submit', views.submit, name="submit"), ## new
+    path(r'', views.show_form, name="show_form"), ## default url (http://127.0.0.1:8000/formdata/) views.show_form runs function show_form
+
+    # when website reaches this link (http://127.0.0.1:8000/formdata/submit) 
+    # views.submit runs function submit which processes the form submission using the POST method to show "formdata/confirmation.html" template
+    path(r'submit', views.submit, name="submit"), 
 ]
