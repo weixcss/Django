8fd4b01f09c9596e81aed5abe6c2efd351084aa9
weixcss <weixcss@gmail.com>
2024-09-30 14:06:58 -0400

    restaurant finalized

--- restaurant/views.py
+++ restaurant/views.py
@@ -14,22 +14,25 @@ def order(request):
 def confirmation(request):
     if request.method == 'POST':
         menu_items = request.POST.getlist('menu_item')
-        prices = request.POST.getlist('item_price')
-        ordered_items = list(zip(menu_items, prices))  # Ensure it's a list to iterate in the template
-        total_price = sum(float(price) for price in prices)
-        
+        prices = [request.POST.get('item_price_' + item) for item in menu_items]
+        ordered_items = list(zip(menu_items, prices))  # This will pair each item with its price
+
+        total_price = sum(float(price) for price in prices if price is not None)
+
         customer_info = {
             'name': request.POST.get('name'),
             'phone': request.POST.get('phone'),
             'email': request.POST.get('email')
         }
         
-        special_instructions = request.POST.get('instructions', 'No special instructions provided.')  # Get special instructions
+        special_instructions = request.POST.get('instructions', 'No special instructions provided.')
 
         current_time = time.time()
+        current_time_converted = time.strftime('%H:%M', time.localtime(current_time))
         ready_time = time.strftime('%H:%M', time.localtime(current_time + random.randint(1800, 3600)))
 
         context = {
+            'current_time': current_time_converted,
             'ordered_items': ordered_items,
             'total_price': total_price,
             'customer_info': customer_info,
@@ -39,4 +42,3 @@ def confirmation(request):
         return render(request, 'restaurant/confirmation.html', context)
     else:
         return render(request, 'restaurant/order.html', {'error_message': 'Invalid request method.'})
-
