9b6f677ca2ccb389a812eb85a300d1c25d5d892a
weixcss <weixcss@gmail.com>
2024-09-30 01:58:43 -0400

    created restaurants app

--- formdata/views.py
+++ formdata/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse, redirect
 
 # Create your views here.
 def show_form(request):
@@ -6,11 +6,13 @@ def show_form(request):
     template_name = "formdata/show_form.html"
     return render(request, template_name)
 
-def submit(request):
+def submit(request): # request is the HttpRequest object, <WSGIRequest: POST '/formdata/submit'>
     '''Process the form submission, and generate a result.'''
+
     template_name = "formdata/confirmation.html"
-    # read the form data into python variables:
-    if request.POST:
+
+    if request.POST: #request.POST prints, <QueryDict: {'name': ['John'], 'favorite_color': ['blue']}>
+                     #<QueryDict: {'csrfmiddlewaretoken': ['T95JUkwLJoMYv3G8SPfIxKFw6GG6gKBFHYZzTJ0U63AGlTXSZfP4LA7DloLq6wBk'], 'name': ['wei'], 'favorite_color': ['orange']}>
         name = request.POST['name']
         favorite_color = request.POST['favorite_color']
         context = {
@@ -18,4 +20,9 @@ def submit(request):
             'favorite_color':  favorite_color,
             
         }
-    return render(request, template_name, context=context)
+        return render(request, template_name, context=context)
+
+    
+    #template_name = "formdata/show_form.html"
+    #return render(request, template_name)
+    return redirect('show_form') # redirect to the show_form view function, which will render the show_form.html template, url http://127.0.0.1:8000/formdata/
