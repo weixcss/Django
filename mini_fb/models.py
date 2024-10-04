dfa672ab22e0c7ce30d4522282ea97d07fa2a874
weixcss <weixcss@gmail.com>
2024-10-04 15:26:46 -0400

    docstring for functions added

--- mini_fb/models.py
+++ mini_fb/models.py
@@ -3,6 +3,16 @@ from django.db import models
 # Create your models here.
 
 class Profile(models.Model):
+    """
+    A model representing a user's profile with personal details.
+
+    Attributes:
+    - first_name: first name of user (max 30 characters).
+    - last_name: last name of user (max 30 characters).
+    - city: city of user (max 50 characters).
+    - email: email address of user
+    - profile_image_url: image address of user's profile picture
+    """
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     city = models.CharField(max_length=50)
@@ -10,4 +20,7 @@ class Profile(models.Model):
     profile_image_url = models.URLField()
 
     def __str__(self):
+        """
+        Returns a string of the user's first name and last name.
+        """
         return f"{self.first_name} {self.last_name}"
--- mini_fb/views.py
+++ mini_fb/views.py
@@ -5,6 +5,14 @@ from django.views.generic import ListView
 from .models import Profile
 
 class ShowAllProfilesView(ListView):
+    """
+    Class-based view that inherits from Django's ListView to display all existing Profile records from admin.
+    
+    Attributes:
+    - model: used to fetch the data (Profile).
+    - template_name: HTML template used to render the view ('mini_fb/show_all_profiles.html').
+    - context_object_name: context variable name used in the template ('profiles').
+    """
     model = Profile
     template_name = 'mini_fb/show_all_profiles.html'
     context_object_name = 'profiles'
