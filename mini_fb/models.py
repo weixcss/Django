fe11696b70a813ff0cb772e684b3b978298c0905
weixcss <weixcss@gmail.com>
2024-10-04 15:18:13 -0400

    mini_fb finished

--- mini_fb/models.py
+++ mini_fb/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Profile(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    city = models.CharField(max_length=50)
+    email = models.EmailField()
+    profile_image_url = models.URLField()
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
